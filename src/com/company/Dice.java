7a75f80d94f0e3e7409801535b2e519d9e9e339a
sujin16 <tnwls9712@ajou.ac.kr>
2020-02-09 23:33:08 +0900

    use random field

--- src/com/company/Dice.java
+++ src/com/company/Dice.java
@@ -4,33 +4,17 @@ import org.omg.CORBA.PUBLIC_MEMBER;
 
 import java.util.Random;
 
-public class Dice extends Random {
-    public Dice(){
-        super(314159L);
-    }
-    public Dice(long seed){ super(seed);}
+public class Dice {
+    private final Random random;
 
-    @Override
-    public int nextInt() {
-        return nextInt(6) +1; //1 ~ 6 random int
+    public Dice() {
+        this.random =new Random(314159L);
     }
-
-    //의문점 : 상속 거부를 이렇게 많이하는데 random class을 상속 하는 것이 맞는 것인가 ??
-    //상속 거부
-    @Override
-    public void nextBytes(byte[] bytes) {throw  new UnsupportedOperationException(); }
-    //상속 거부
-    @Override
-    public boolean nextBoolean() {
-        throw  new UnsupportedOperationException();
+    public Dice(int seed) {
+        this.random =new Random(seed);
     }
-    //상속 거부
-    @Override
-    public float nextFloat() {throw  new UnsupportedOperationException(); }
-    //상속 거부
-    @Override
-    public double nextDouble() { throw  new UnsupportedOperationException();}
-    //상속 거부
-    @Override
-    public synchronized double nextGaussian() {throw  new UnsupportedOperationException(); }
+    public int nextInt(){
+        return random.nextInt(6)+1;
+    }
+    public void setSeed(int seed){ random.setSeed(seed);}
 }
